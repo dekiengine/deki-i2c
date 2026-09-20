ce89c4c72288a44b8f8df35c580d93c5d9f38e47
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:04:29 +0200

    Docs for deki-i2c from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -30,8 +30,10 @@ var NAVTREE =
       [ "I2CBusComponent", "index.html#c-i2cbuscomponent", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md2", [
-      [ "0.15.0", "index.html#autotoc_md3", null ],
-      [ "Changed", "index.html#autotoc_md4", null ]
+      [ "0.16.0", "index.html#autotoc_md3", null ],
+      [ "Changed", "index.html#autotoc_md4", null ],
+      [ "0.15.0", "index.html#autotoc_md5", null ],
+      [ "Changed", "index.html#autotoc_md6", null ]
     ] ]
   ] ]
 ];
